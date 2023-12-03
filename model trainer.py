59edfb86521ba4d1232ea91659c5e1b91a2899d3
Dean <98725600+deanMoisakos@users.noreply.github.com>
2023-12-03 16:15:33 -0500

    Add files via upload

---
+++ model trainer.py
@@ -0,0 +1,130 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+from torchvision.datasets import ImageFolder
+import os
+
+torch.manual_seed(42) #seed for random generation
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu") #use cpu for the model
+
+data_path = "c:/Users/c000832213/Documents/DeanStuff/snek/Pill Identifier/dataset" #data path for dataset      
+classes = ['pill', 'non_pill'] #folder names for classes
+
+#additional transformation for data augmentation
+augmentation_transform = transforms.Compose([
+    transforms.RandomRotation(20),
+    transforms.RandomHorizontalFlip(),
+])
+
+#combine both transforms
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.RandomApply([augmentation_transform, transforms.ToTensor()]),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+])
+
+if __name__ == '__main__':
+    #transformation for data augmentation
+    augmentation_transform = transforms.Compose([
+        transforms.RandomRotation(20),
+        transforms.RandomHorizontalFlip(),
+    ])
+
+    #transformation for consistent processing
+    transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.RandomApply([augmentation_transform]),
+    transforms.ToTensor(),  # Convert PIL image to tensor before normalization
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+])
+
+    #load dataset with transformations applied
+    dataset = ImageFolder(root=data_path, transform=transform)
+
+    #split total data set into training and validation
+    train_size = int(0.8 * len(dataset)) #80% of data goes to training, rest goes to validation
+    val_size = len(dataset) - train_size
+    train_dataset, val_dataset = torch.utils.data.random_split(dataset, [train_size, val_size])
+
+    #training and validation loaders
+    train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=32)
+
+    model = torch.hub.load('pytorch/vision:v0.9.0', 'resnet18', weights=None)  #model resnet18 is loaded from pytorch
+    num_features = model.fc.in_features
+    model.fc = nn.Linear(num_features, len(classes))
+    model.to(device)
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=0.001)  #optimize models parameters during training
+
+# Training loop
+num_epochs = 7  #number of training sessions the model will perform
+threshold = 0.75  #accuracy threshold for model
+
+for epoch in range(num_epochs):
+    model.train()  #begin model training
+    train_loss = 0.0  #variables to track models loss, correct, and total
+    train_correct = 0
+    train_total = 0
+
+    for images, labels in train_loader:
+        images = images.to(device)
+        labels = labels.to(device)
+
+        optimizer.zero_grad()
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        probabilities = torch.softmax(outputs, dim=1)
+        max_prob, predicted = torch.max(probabilities, 1)
+
+        #check if probability of being a pill exceeds threshold
+        for i in range(len(predicted)):
+            if max_prob[i].item() > threshold:
+                pill_label = classes[predicted[i].item()]
+            else:
+                pill_label = 'Not a Pill'  
+
+        train_loss += loss.item()
+        train_total += labels.size(0)
+        train_correct += (predicted == labels).sum().item()
+
+    train_accuracy = 100.0 * train_correct / train_total  #calculate the training accuracy as a percentage
+    avg_train_loss = train_loss / len(train_loader)
+
+
+    #evaluation on validation set
+    model.eval()  #load the validation dataset and evaluate the models performance
+    val_loss = 0.0
+    val_correct = 0
+    val_total = 0
+
+    with torch.no_grad():
+        for images, labels in val_loader:
+            images = images.to(device)
+            labels = labels.to(device)
+
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+
+            _, predicted = torch.max(outputs.data, 1)  #update validation statistics
+            val_loss += loss.item()
+            val_total += labels.size(0)
+            val_correct += (predicted == labels).sum().item()
+
+    val_accuracy = 100.0 * val_correct / val_total  #calculate validation accuracy as a percentage
+    avg_val_loss = val_loss / len(val_loader)
+
+    #print training statistics
+    print(f"Epoch {epoch + 1}/{num_epochs} | Train Loss: {avg_train_loss:.4f} | Train Acc: {train_accuracy:.2f}% | "
+          f"Val Loss: {avg_val_loss:.4f} | Val Acc: {val_accuracy:.2f}%")
+
+#save the trained model
+model_path = os.path.join(data_path, "pill_classifier.pth")
+torch.save(model.state_dict(), model_path)
---
+++ pill identifier.py
@@ -0,0 +1,192 @@
+import cv2
+import torch
+import torch.nn as nn
+import torchvision.transforms as transforms
+from PIL import Image
+from torchvision.models import resnet18
+import numpy as np
+
+# Define the modified model architecture
+class CustomResNet(nn.Module):
+    def __init__(self, num_classes):
+        super(CustomResNet, self).__init__()
+        self.resnet = resnet18(weights=None)
+        num_features = self.resnet.fc.in_features
+        self.resnet.fc = nn.Linear(num_features, num_classes)
+
+    def forward(self, x):
+        return self.resnet(x)
+
+#set the for the model to run on
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+#load the saved model from its file location
+model_path = "c:/Users/c000832213/Documents/DeanStuff/snek/Pill Identifier/dataset/pill_classifier.pth"
+saved_state_dict = torch.load(model_path)
+
+#create the model using resnet and move it to the device
+model = CustomResNet(num_classes=2)
+model.to(device)
+
+#loads the current save state into the model and puts it into evaluation mode
+model.load_state_dict(saved_state_dict, strict=False)
+model.eval()
+
+#set up the camera, accesing the default
+cap = cv2.VideoCapture(0)
+
+#define the image transformations
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+])
+
+#define the two classes pill and non pill
+class_labels = ['non_pill', 'pill']
+
+#track object variable, false on startup
+track_object = False
+
+#contours variable, empty array
+contours = []
+
+#the region for text, to be used to later place and mask it
+text_region = [(0, 0), (350, 40)]  
+
+#merging threshold for rectangles
+merge_threshold = 50
+
+#create the window
+cv2.namedWindow("Pill Identification and Object Tracking")
+
+while True:
+    #capture camera frame
+    ret, frame = cap.read()
+
+    #image processing
+    pil_image = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+    pil_image = transform(pil_image)
+    input_image = pil_image.unsqueeze(0).to(device)
+
+    #create mask to cover text region
+    mask = np.zeros(frame.shape[:2], dtype=np.uint8)
+    cv2.rectangle(mask, text_region[0], text_region[1], (255), -1)
+
+    #apply mask to the text frame and make it black
+    frame[mask > 0] = [0, 0, 0]
+
+    #run pill idenfitication
+    with torch.no_grad():
+        output = model(input_image)
+        _, predicted = torch.max(output, 1)
+    if track_object:
+        #pill identification. Process and output results
+        pill_label = class_labels[predicted.item()]
+        #if a pill, say so and make text green
+        if pill_label == 'pill':
+            pill_text = 'Pill'
+            pill_color = (0, 255, 0)
+            track_object = True
+        #if not a pill, say so and make text red
+        elif pill_label == 'non_pill':
+            pill_text = 'Not a Pill'
+            pill_color = (0, 0, 255)
+            track_object = False        
+        
+        #add indentification text
+        cv2.putText(frame, pill_text, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, pill_color, 2)
+    
+    if not track_object and len(contours) == 0:
+        pill_text = 'No Object Detected'
+        pill_color = (0,0,255)
+        cv2.putText(frame, pill_text, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, pill_color, 2)
+
+    #cv2.imshow("Pill Identification and Object Tracking", frame)
+
+    #convert frame to color frame
+    hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    #define lower and upper color range
+    lower_color = np.array([40, 50, 50])
+    upper_color = np.array([80, 255, 255])
+
+    #create mask based on color image
+    mask = cv2.inRange(hsv_frame, lower_color, upper_color)
+
+    #find contours in mask
+    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    
+    #min threshold to prioritize large contours
+    min_area_threshold = 1000
+
+    #filter contours by area to search for largest contour
+    large_contours = [cnt for cnt in contours if cv2.contourArea(cnt) > min_area_threshold]
+
+    if len(large_contours) > 0:
+        #sort contours by area to look for largest contour
+        large_contours = sorted(large_contours, key=cv2.contourArea, reverse=True)
+        largest_contour = large_contours[0]
+    
+    if not track_object or len(contours) == 0:
+        #check for contours
+        if len(contours) > 0:
+            #initialize the list to store the individual points
+            points = []
+
+            for contour in contours:
+                #get contour points
+                contour_points = cv2.convexHull(contour)
+
+                #add contour points to list
+                points.extend(contour_points)
+
+            if len(points) > 0:
+                #convert points to array
+                points = np.array(points)
+
+                #calculate points of rectangle
+                rect = cv2.minAreaRect(points)
+                box = cv2.boxPoints(rect)
+                box = np.array(box, dtype=np.intp)
+
+                #draw the box
+                if len(box) > 0:
+                    cv2.drawContours(frame, [box], 0, (0, 255, 0), 2)
+
+
+                #and set track object to true
+                track_object = True
+            else:
+                #track object if no points are found
+                track_object = False
+        else:
+            #track object is false if no countours are found
+            track_object = False
+    else:
+        #if track_object is True
+        for contour in contours:
+            contour_points = cv2.convexHull(contour)
+            points = np.concatenate([points, contour_points])
+
+        if len(points) > 0:
+            points = np.array(points)
+            rect = cv2.minAreaRect(points)
+            box = cv2.boxPoints(rect)
+            box = np.array(box, dtype=np.intp)
+            if len(box) > 0:
+                cv2.drawContours(frame, [box], 0, (0, 255, 0), 2)
+
+        else:
+            #track object is false if no points are found
+            track_object = False
+
+    #display the frame
+    cv2.imshow("Pill Identification and Object Tracking", frame)
+
+    #exit the loop on 'q' key press
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
