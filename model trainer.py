4d37b5cdd2cb98d3c89d9b55cb1fc559ead84aa2
Dean <98725600+deanMoisakos@users.noreply.github.com>
2023-12-04 14:00:25 -0500

    Update model trainer.py

--- model trainer.py
+++ model trainer.py
@@ -62,7 +62,7 @@ if __name__ == '__main__':
     optimizer = optim.Adam(model.parameters(), lr=0.001)  #optimize models parameters during training
 
 # Training loop
-num_epochs = 7  #number of training sessions the model will perform
+num_epochs = 5  #number of training sessions the model will perform
 threshold = 0.75  #accuracy threshold for model
 
 for epoch in range(num_epochs):
